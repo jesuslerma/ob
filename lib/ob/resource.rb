e07fd1b7f5db6f88650c806b28c5b1838509a184
hyde <hyde@linux-dw8w.site>
2015-01-27 11:19:16 -0600

    add validation on cust url

--- lib/ob/resource.rb
+++ lib/ob/resource.rb
@@ -1,12 +1,7 @@
 module Ob
 	class Resource < ObObject
-		attr_reader custum_name
 		def self.url()
-			if custum_name.nil?
-				"/#{CGI.escape(self.class_name)}"
-			else
-				"/#{CGI.escape(custum_name)}"
-			end
+			"/#{CGI.escape(self.class_name)}"
 		end
 		def url
 			return [self.class.url, id].join('/')
